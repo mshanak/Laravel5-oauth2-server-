ee149ee2bab8df105b08302decff9190166a6728
Mahdi <mshanak@gmail.com>
2015-05-13 14:00:17 +0300

    update seed file
    update the seed file , the
    client_id =GXvOWazQ3lA6YSaFji
    secret=GXvOWazQ3lA.6/YSaFji

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -29,7 +29,7 @@ class DatabaseSeeder extends Seeder {
          DB::table('oauth_clients')->delete();
          //insert some dummy records
          DB::table('oauth_clients')->insert(array(
-             array('id'=>Hash::make('client1'),'secret'=>Hash::make('pas2123'),'name'=>'client 1')            
+             array('id'=>'GXvOWazQ3lA6YSaFji','secret'=>'GXvOWazQ3lA.6/YSaFji','name'=>'client 1')            
 
           ));
   		$this->command->info('oauth_clients seeded!');
--- readme.md
+++ readme.md
@@ -2,3 +2,13 @@
 
 this scafold project based on [PHP OAuth 2.0 Server for Laravel] (https://github.com/lucadegasperi/oauth2-server-laravel)
 
+1. clone this project 
+ git clone https://github.com/mshanak/Laravel5-oauth2-server-.git foldername
+
+2. then run 
+composer update
+
+3. then run 
+php artisan migrate
+
+if "Nothing to migrate." message apeared , run "php artisan migrate:reset"
